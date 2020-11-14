241827bf38d881d803c9e5a04f7a9908ce913bea
Sebastian M. L <smolina624@gmail.com>
2020-11-14 18:26:38 +0100

    Create service package

--- resources/cardb_requests_test.py
+++ resources/cardb_requests_test.py
@@ -1,7 +1,11 @@
 from typing import Dict
 import requests
 import json
+from base64 import b64encode
+from requests.adapters import Response
 
+from requests.auth import HTTPBasicAuth
+from requests import auth
 
 '''
 This exercises of the book is proposed to be done with Postman or cURL.
@@ -10,61 +14,88 @@ For consume RESTful APIs HTTP requests, I prefered create a little script about.
 
 port = "8081"
 url_api_endpoint = "http://localhost:{port}/api".format(port=port)
+api_key = b64encode(s=b"CarDatabase:abcd1234").decode('UTF-8')
 
 
 class CarDatabaseRequest(object):
+
     def __init__(self, *args):
         super(CarDatabaseRequest, self).__init__(*args)
+        self.__session()
 
-    @staticmethod
+    def __session(self) -> requests.Session:
+        session = requests.Session()
+        session.headers.update(self.__get_headers())
+        session.auth = HTTPBasicAuth('CarDatabase', 'abcd1234')
+        session.verify = False
+        self.__session = session
+
+    @ staticmethod
     def __get_headers():
         headers = {
-            'content-type': 'application/json',
-            'Cache-Control': "no-cache"
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            # 'Authorization': 'Basic {api_key}'.format(api_key=api_key)
         }
         return headers
 
+    def __start_get_request(self, endpoint: str):
+        response = self.__session.get(
+            url=endpoint,
+        )
+        return response
+
     def __start_post_request(self, endpoint: str, payload: Dict):
-        response = requests.post(
+        response = self.__session.post(
             url=endpoint,
             data=json.dumps(payload),
             headers=self.__get_headers(),
-            verify=False
+            # auth=HTTPBasicAuth('CarDatabase', 'abcd1234')
         )
         return response
 
-    @staticmethod
-    def __start_patch_request(endpoint: str, payload: Dict):
-        response = requests.patch(
+    @ staticmethod
+    def __start_patch_request(self, endpoint: str, payload: Dict):
+        response = self.__session.patch(
             url=endpoint,
             data=json.dumps(payload),
         )
         return response
 
-    @staticmethod
-    def __start_delete_request(endpoint: str):
-        response = requests.delete(
+    @ staticmethod
+    def __start_delete_request(self, endpoint: str):
+        response = self.__session.delete(
             url=endpoint
         )
         return response
 
-    def __end_request(self, response):
-        print(response)
-        self.json_printer(
-            response.json()
-        )
-        return response.json()
+    def __end_request(self, response: Response):
+        print(f"Request - status code: {response.status_code}")
+        print(response.request.headers)
+        print(response.cookies)
+        try:
+            self.json_printer(
+                response.json()
+            )
+            return response.json()
+        except json.decoder.JSONDecodeError as eMsg:
+            self.json_printer(
+                response.json()
+            )
+            return response.json()
+            print(f"json.decoder.JSONDecodeError: {eMsg}")
 
     def get(self, endpoint):
-        print(f"GET: {endpoint}")
-        response = requests.get(
-            url=endpoint
+        print(f"\nGET: {endpoint}")
+        response = self.__start_get_request(
+            endpoint=endpoint
         )
-        self.json_printer(response)
-        return response.json()
+        return self.__end_request(
+            response=response
+        )
 
     def post(self, endpoint, payload):
-        print(f"POST: {endpoint}")
+        print(f"\nPOST: {endpoint}")
         response = self.__start_post_request(
             endpoint=endpoint,
             payload=payload
@@ -74,7 +105,7 @@ class CarDatabaseRequest(object):
         )
 
     def patch(self, endpoint, payload):
-        print(f"PATCH: {endpoint}")
+        print(f"\nPATCH: {endpoint}")
         response = self.__start_patch_request(
             endpoint=endpoint,
             payload=payload
@@ -91,73 +122,64 @@ class CarDatabaseRequest(object):
         print(response)
         print(response.text)
         return response
-        
 
     def get_api(self):
         print(f"\nGET : API")
-        json_response = self.get(
-            url=url_api_endpoint
+        return self.get(
+            endpoint=url_api_endpoint
         )
-        return json_response
 
     def get_cars(self):
         print(f"\nGET : Cars")
-        json_response = requests.get(url="{url}/cars"
-                                     .format(
-                                         url=url_api_endpoint
-                                     )
-                                     ).json()
-        self.json_printer(json_response)
+        endpoint = ("{url}/cars"
+                    .format(
+                        url=url_api_endpoint
+                    ))
+        return self.get(endpoint=endpoint)
 
     def get_owners(self):
         print(f"\nGET : Owners")
-        json_response = requests.get(url="{url}/owners"
-                                     .format(
-                                         url=url_api_endpoint
-                                     )
-                                     ).json()
-
-        self.json_printer(json_response)
+        endpoint = ("{url}/owners"
+                    .format(
+                        url=url_api_endpoint
+                    ))
+        return self.get(endpoint=endpoint)
 
     def get_car(self, car_id):
         print(f"\nGET : Car")
-        json_response = requests.get(url="{url}/cars/{car_id}"
-                                     .format(
-                                         url=url_api_endpoint,
-                                         car_id=car_id
-                                     )
-                                     ).json()
-        self.json_printer(json_response)
+        endpoint = ("{url}/cars/{car_id}"
+                    .format(
+                        url=url_api_endpoint,
+                        car_id=car_id
+                    ))
+        return self.get(endpoint=endpoint)
 
     def get_owner(self, owner_id):
         print(f"\nGET : Owner")
-        json_response = requests.get(url="{url}/owners/{owner_id}"
-                                     .format(
-                                         url=url_api_endpoint,
-                                         owner_id=owner_id
-                                     )
-                                     ).json()
-        self.json_printer(json_response)
+        endpoint = ("{url}/owners/{owner_id}"
+                    .format(
+                        url=url_api_endpoint,
+                        owner_id=owner_id
+                    ))
+        return self.get(endpoint=endpoint)
 
     def get_owner_from_car(self, car_id):
         print(f"\nGET : Owner from car")
-        json_response = requests.get(url="{url}/cars/{car_id}/owner"
-                                     .format(
-                                         url=url_api_endpoint,
-                                         car_id=car_id
-                                     )
-                                     ).json()
-        self.json_printer(json_response)
+        endpoint = ("{url}/cars/{car_id}/owner"
+                    .format(
+                        url=url_api_endpoint,
+                        car_id=car_id
+                    ))
+        return self.get(endpoint=endpoint)
 
     def get_cars_from_owner(self, owner_id):
         print(f"\nGET : Cars from owner")
-        json_response = requests.get(url="{url}/owners/{owner_id}/cars"
-                                     .format(
-                                         url=url_api_endpoint,
-                                         owner_id=owner_id
-                                     )
-                                     ).json()
-        self.json_printer(json_response)
+        endpoint = ("{url}/owners/{owner_id}/cars"
+                    .format(
+                        url=url_api_endpoint,
+                        owner_id=owner_id
+                    ))
+        return self.get(endpoint=endpoint)
 
     def post_car(self, params):
         print(f"\nPOST : Car")
@@ -266,8 +288,8 @@ def run_getters(carDBRequest: CarDatabaseRequest = CarDatabaseRequest()):
     # carDBRequest.get_api()
     carDBRequest.get_cars()
     # carDBRequest.get_owners()
-    # carDBRequest.get_car(id=3)
-    # carDBRequest.get_owner(id=1)
+    # carDBRequest.get_car(car_id=3)
+    # carDBRequest.get_owner(owner_id=1)
     # carDBRequest.get_owner_from_car(car_id=3)
     # carDBRequest.get_cars_from_owner(owner_id=2)
 
@@ -285,11 +307,11 @@ def run_deletes(carDBRequest: CarDatabaseRequest = CarDatabaseRequest()):
 
 
 def main():
-    # run_getters()
-    # run_posts()
+    run_getters()
+    run_posts()
     # run_patches()
     # run_getters()
-    run_deletes()
+    # run_deletes()
 
 
 if __name__ == '__main__':
